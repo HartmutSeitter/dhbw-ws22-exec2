fbfdf014d3a4fdbe12786330c3cf3d16c06e47b6
Hartmut Seitter <Hartmut.Seitter@googlemail.com>
2022-10-16 14:45:26 +0200

    Add files via upload

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,1 +1,16 @@
+import React from "react";
 
+// hs002 receive the list of comments
+const CommentList = ({ comments }) => {
+
+  // hs002 delete this stuff
+  
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
